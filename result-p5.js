40aed2ef953e2742d52bdd0c542f23f712e82675
danyizen <danyizeng@gmail.com>
2023-04-17 14:58:33 -0400

    in-class first trial

--- result-p5.js
+++ result-p5.js
@@ -1,7 +1,35 @@
-async function result() {
-    const gachaBtn = document.querySelector('#gacha')
-    gachaBtn.addEventListener('click', () => {
-        
-    }
+// function result() {
+//     const gachaBtn = document.querySelector('#result')
+//     gachaBtn.addEventListener('click', () => {
+
+async function goToList (){
+    //on canvas
+    let canvas = document.querySelector('canvas');
+    
+    // let data = localStorage.getItem('NYTListNames');
+    // let goToList = JSON.parse(data); always the 58-item
+
+    // .json(): the result of taking JSON as input and parsing it to produce a JavaScript object. 
+    const results = await fetch("https://api.nytimes.com/svc/books/v3/lists/current/humor.json?api-key=7UwAx6ld8Cu8YoQcUnaSUKrjhagUAPy7");
+    const data = await results.json()
+    console.table(data);
+    
+    //stringfy data & parse
+    
+
+    //get 4 strings: 1) rank, 2) book name, 3) summary, 4) photo
+
+    //form new Array[][]
+
+    //randomize: choose one[] from the array
+
+    //get the chosen one's information visualized on canvas, w/ the photo of of p5.js effect
 }
-document.addEventListener('DOMContentLoaded', async () => result());
+
+//     })
+// }
+// document.addEventListener('DOMContentLoaded', () => result());
+// how to add async function in sync?
+
+
+document.addEventListener('DOMContentLoaded', async () => goToList());
